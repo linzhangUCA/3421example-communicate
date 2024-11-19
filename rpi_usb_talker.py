9e63291170512c879812a275fcd08399f3c9ce8d
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2024-11-19 06:39:25 -0600

    Update rpi_usb_talker.py

--- rpi_usb_talker.py
+++ rpi_usb_talker.py
@@ -2,16 +2,16 @@ import serial
 from time import sleep
 
 # SETUP
-ser = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
-print(ser.name)
+messenger = serial.Serial(port='/dev/ttyACM0', baudrate=115200)
+print(messenger.name)
 
 # LOOP
 for i in range(10):
     msg = f"Hello from RPi: {i}\n".encode('utf-8')
-    ser.write(msg)
-    if ser.inWaiting() > 0:
-        pico_data = ser.readline()
-        pico_data = pico_data.decode('utf-8', 'ignore')
-        print(pico_data)
+    messenger.write(msg)
+    # if messenger.inWaiting() > 0:
+    #     pico_data = messenger.readline()
+    #     pico_data = pico_data.decode('utf-8', 'ignore')
+    #     print(pico_data)
     sleep(1)
-ser.close()
+messenger.close()
